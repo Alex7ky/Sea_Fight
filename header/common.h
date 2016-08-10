3b8e9c323df1621a639b3d98f5ae26908f26765e
Alex7ky <alex7ky@yandex.ru>
2016-08-10 15:26:36 +0700

    Изменение структуры, добавление макроса CELL_SHIP

--- header/common.h
+++ header/common.h
@@ -9,23 +9,12 @@
 #define CELL_FREE   0       // Удара по ячейке не было
 #define CELL_MISS   1       // Удар мимо
 #define CELL_HIT    2       // Попадение по ячейке корабля
-
-/**
- * Структура, описывающая корабль (его положение)
- */
-struct ship
-{
-    int startx;     // Координата X начала
-    int starty;     // Координата Y начала
-    int endx;       // Координата X конца
-    int endy;       // Координата Y конца
-}
+#define CELL_SHIP   3       // Корабль
 
 /**
  * Структура, описывающая игровое поле
  */
 struct play_field
 {
-    char field[FIELD_COLS][FIELD_LINES];    // Удары по полю
-    struct ship ships[CNT_SHIPS];           // Корабли
+    char field[FIELD_COLS][FIELD_LINES];    // Игровое поле
 }
