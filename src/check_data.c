7ac1f1e570c132a55f0ccf573c97417b4db5139c
biunovich <biunovich@gmail.com>
2016-08-12 12:45:52 +0700

    Change logic

--- src/check_data.c
+++ src/check_data.c
@@ -1,16 +1,25 @@
 #include "../header/net.h"
 #include "../header/common.h"
 #include "../header/check_data.h"
+#include "../header/server.h"
 
-void Check_data (struct CLT_DATA clt_data, struct play_field *private_field, struct play_field *user_field)
+void Check(CLT_DATA *clt_data,char ** map_pub,char **map_prv)
 {
-	switch(private_field->field[clt_data.posx][clt_data.posy])
+	switch(map_prv[clt_data.posx][clt_data.posy])
 	{
 		case CELL_FREE:
-		user_field->field[clt_data.posx][clt_data.posy] = CELL_MISS;
+		map_pub[clt_data.posx][clt_data.posy] = CELL_MISS;
 		break;
 		case CELL_SHIP:
-		user_field->field[clt_data.posx][clt_data.posy] = CELL_HIT;
+		map_pub[clt_data.posx][clt_data.posy] = CELL_HIT;
 		break;
 	}
+}
+
+void Check_data (CLT_DATA *clt_data, struct play_field * cli_1 ,struct play_field cli_2, int id)
+{
+	if (id)
+		Check(clt_data,&cli_1->pub,&cli_2->prv);
+	else
+		Check(clt_data,&cli_2->pub,&cli_1->prv);
 }
