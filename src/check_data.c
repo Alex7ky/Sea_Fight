f5549f068bfe9663ebba660f500394847eff5147
Dylgyrzhapov Buin <biunovich@gmail.com>
2016-08-12 16:43:15 +0800

    Change logic again

--- src/check_data.c
+++ src/check_data.c
@@ -3,25 +3,17 @@
 #include "../header/check_data.h"
 #include "../header/server.h"
 
-void Check(CLT_DATA *clt_data,char ** map_pub,char **map_prv)
+void Check_data(CLT_DATA *clt_data, struct play_field *field , int *current_ships)
 {
-	switch(map_prv[clt_data->posx][clt_data->posy])
+	switch(field->prv[clt_data->posx][clt_data->posy])
 	{
 		case CELL_FREE:
-		map_pub[clt_data->posx][clt_data->posy] = CELL_MISS;
-		map_prv[clt_data->posx][clt_data->posy] = CELL_MISS;
+		field->pub[clt_data->posx][clt_data->posy] = CELL_MISS;
+		field->prv[clt_data->posx][clt_data->posy] = CELL_MISS;
 		break;
 		case CELL_SHIP:
-		map_pub[clt_data->posx][clt_data->posy] = CELL_HIT;
-		map_prv[clt_data->posx][clt_data->posy] = CELL_HIT;
+		field->pub[clt_data->posx][clt_data->posy] = CELL_HIT;
+		field->prv[clt_data->posx][clt_data->posy] = CELL_HIT;
 		break;
 	}
-}
-
-void Check_data (CLT_DATA *clt_data, struct play_field * cli_1 ,struct play_field cli_2, int id)
-{
-	if (id)
-		Check(clt_data,cli_1->pub,cli_2->prv);
-	else
-		Check(clt_data,cli_2->pub,cli_1->prv);
 }
